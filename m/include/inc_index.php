fd9e29eea617549f42c9b9eba3473e12c61b3a80
greezen <852820276@qq.com>
2017-06-07 14:55:59 +0800

    20170607

--- m/include/inc_index.php
+++ m/include/inc_index.php
@@ -5,6 +5,9 @@ if ( CURSCRIPT != "wap" )
     exit( "FORBIDDEN" );
 }
 $categories = $db->getAll('SELECT * FROM `my_category` WHERE parentid=0 AND if_view=2 ORDER BY catorder ASC');
-
+if (mgetcookie('post') == 1) {
+    $url = 'http://'.$_SERVER['HTTP_HOST'] . '/m/index.php?mod=post&cityid='.$cityid;
+    header('Location:'.$url);exit;
+}
 include( mymps_tpl( "index" ) );
 ?>
